b0704b706742c0ec60509c5a4b90e2f02523657b
Julio Donado <julio22567@hotmail.com>
2024-05-29 07:51:26 -0500

    feat: Add Auth module, controller, service, and DTOs
    This commit adds the Auth module, controller, service, and DTOs to the project. The Auth module is responsible for handling authentication-related functionality. It includes endpoints for creating, updating, and deleting auth entities. The Auth controller handles the HTTP requests and delegates the logic to the Auth service. The Auth service contains the business logic for handling auth operations. The CreateAuthDto and UpdateAuthDto are used for validating and transferring data between the client and server.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,53 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { readFileSync } from 'fs';
+import { join } from 'path';
+import { json } from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  let app: INestApplication;
+  if (process.env.NODE_ENV === 'prd') {
+    //? Configuracion de la aplicacion de SSL con una clave privada string y un certificado
+    const httpsOptions = {
+      passphrase: 'unibolbaq99',
+      pfx: readFileSync(join('C:', 'ssl', 'unibol08282023.pfx')),
+    };
+
+    app = await NestFactory.create(AppModule, { httpsOptions });
+
+    //? Configuracion de CORS
+  app.enableCors({
+    origin: [
+      'http://localhost:5174',
+      'http://localhost:5173',
+      'http://localhost:6500',
+      'http://192.168.104.15:5173',
+      'http://localhost:53257',
+      'https://oficina.unibol.com.co:8089',
+    ],
+    methods: '*',
+    allowedHeaders: '*',
+  });
+
+  } else {
+    app = await NestFactory.create(AppModule, {
+      bodyParser: false
+    });
+    app.use(json({ limit: "300mb"}))
+    app.enableCors();
+  }
+
+  //? Configuracion de prefijos
+  app.setGlobalPrefix('api');
+
+  //? Configuracion global de pipes
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();

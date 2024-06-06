8f969df1ce41dc4e01b28c65bee072c054c71b6d
Julio Donado <julio22567@hotmail.com>
2024-06-06 08:42:59 -0500

    chore: Actualizar modelo de canción y servicio de música

--- src/music/music.service.ts
+++ src/music/music.service.ts
@@ -1,7 +1,6 @@
 import { BadRequestException, Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateSongDto } from './dto/create-song.dto';
 import { PrismaClient } from '@prisma/client';
-import { readFileSync } from 'fs';
 
 @Injectable()
 export class MusicService extends PrismaClient implements OnModuleInit {
@@ -19,29 +18,40 @@ export class MusicService extends PrismaClient implements OnModuleInit {
   }
 
   async create(createSongDto: CreateSongDto, file: Express.Multer.File) {
-
     // console.log(file);
-
     //? Validamos que el archivo venga en la petición
     if (!file) {
       throw new BadRequestException('No se ha enviado ningún archivo');
     }
 
     //? Leemos el archivo que se subió al servidor
-    const rutaArchivo = `./${file.path}`;
-    const archivo = readFileSync(rutaArchivo);
+    // const rutaArchivo = `./${file.path}`;
+    // const archivo = readFileSync(rutaArchivo);
     // console.log({ archivo });
 
     //? Guardamos la información en la base de datos
     const song = await this.song.create({
       data: {
+        id: file.filename.split('.').pop(),
         name: createSongDto.name,
         artist: createSongDto.artist,
-        mp3File: archivo,
+        filename: file.filename,
+        mimetype: file.mimetype,
+        path: file.path,
       },
     });
 
-    delete song.mp3File;
+    // delete song.mp3File;
     return song;
   }
+
+  async obtenerCancionPorNombre(name: string) {
+    return this.song.findFirst({
+      where: {
+        name
+      },
+    });
+  }
+
+
 }
